394c3c10164eb1f19b893f07e0b07ea9e3ff81c8
Pragnesh Suthar <85042722+pragnesh-aus@users.noreply.github.com>
2021-10-22 23:40:35 +1100

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\http\Controllers\ProductController;
+use App\http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// public route
+//Route::resource('products', ProductController::class);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+ //Route::post('/products', [ProductController::class, 'store']);
+
+ // protected route
+Route::group(['middleware'=>['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy ']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
